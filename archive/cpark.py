54449af9ba6e0d116d30d52ff295f294ef94c2b7
sszz01 <resios027@gmail.com>
2025-01-25 15:48:56 -0800

    moved directories a bit. colors are now accesible thru data/colors.py

--- archive/cpark.py
+++ archive/cpark.py
@@ -2,7 +2,7 @@ import cv2
 import pickle
 
 rpi_path = "/home/parkai/Downloads/parking-lot.mp4"
-git_path = "media/parking-lot.mp4"
+git_path = "../media/videos/parking-lot.mp4"
 cap = cv2.VideoCapture(git_path) # select your path
 # success,frame=cap.read()
 # frame=cv2.resize(frame,(640,480))+-
---
+++ data/colors.py
@@ -0,0 +1,4 @@
+COLOR_RED = (0, 0, 255)       # Car
+COLOR_BLUE = (255, 255, 0)    # Motorcycle
+COLOR_GREEN = (0, 255, 0) # Bus
+COLOR_ORANGE = (255, 165, 0)  # Truck
