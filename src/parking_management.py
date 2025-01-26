b85d15f4aa5d83152ec768a64fcc07b58f8326fd
sszz01 <resios027@gmail.com>
2025-01-25 16:51:29 -0800

    YOLO parking management implementation fixed

--- src/parking_management.py
+++ src/parking_management.py
@@ -2,20 +2,17 @@ import cv2
 
 from ultralytics import solutions
 
-pts_obj = solutions.ParkingPtsSelection()
+polygon_json_path = "bounding_boxes.json"
 
-# Video capture
 cap = cv2.VideoCapture("../media/videos/parking-lot.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-# Video writer
 video_writer = cv2.VideoWriter("parking_management.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
-# Initialize parking management object
 parking_manager = solutions.ParkingManagement(
     model="models/yolo11n.pt",  # path to model file
-    json_file="bounding_boxes.json",  # path to parking annotations file
+    json_file=polygon_json_path,  # path to parking annotations file
 )
 
 while cap.isOpened():
